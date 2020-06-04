3b9bc9d12eb31156aa6e55b6003942f0c691d05b
Saurabh Chauhan <36479565+starc007@users.noreply.github.com>
2020-06-04 20:24:57 +0530

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const User = require("../model/User");
+const jwt = require("jsonwebtoken");
+const { registerValidation, loginValidation } = require("../validation");
+const bcrypt = require("bcryptjs");
+
+router.post("/register", async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  //Checking existing user
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  //Create new user
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send({ user: user._id });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+//Login
+router.post("/login", async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //Checking existing user
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email not found");
+
+  //password checking
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Password is wrong");
+
+  //create and assign a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header("auth-token", token).send(token);
+
+  //   res.send("Logged in!");
+});
+
+module.exports = router;
